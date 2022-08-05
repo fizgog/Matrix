6aa92d996144b9935ae1a66e2463730c49762943
fizgog <fizgog@users.noreply.github.com>
2022-08-05 22:30:07 +0100

    Update Highscore.asm

--- Src/Highscore.asm
+++ Src/Highscore.asm
@@ -10,24 +10,28 @@
 ;-------------------------------------------------------------------------
 .CheckHighScore
 {
+    LDA #$00
+    STA temp4
+
     LDA score4          
-    CMP high_score + 3    ; Score four check
+    CMP high_score + 3      ; Score four check
     BCC score_lower
     BNE score_higher
     ; score 4 is same
     LDA score3          
-    CMP high_score + 2    ; score three check
+    CMP high_score + 2      ; score three check
     BCC score_lower
     BNE score_higher
     ; score 3 is the same
     LDA score2          
-    CMP high_score + 1    ; score two check
+    CMP high_score + 1      ; score two check
     BCC score_lower
     BNE score_higher
     ; score 2 is the same
     LDA score1          
-    CMP high_score    ; score one check
+    CMP high_score          ; score one check
     BCC score_lower
+    BEQ score_lower         ; score the same
 
 .score_higher
     LDA score4
@@ -38,6 +42,9 @@
     STA high_score + 1,X
     LDA score1
     STA high_score,X
+
+    LDA #$01
+    STA temp4
 
 .score_lower
     RTS
